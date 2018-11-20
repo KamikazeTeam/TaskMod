0d649c2f8f426697a47370d1c9fafd03624c8fd3
KamikazeTeam <joshuasy@gmail.com>
2018-11-20 16:43:29 +0900

    Add files via upload

--- tools/BPobs.py
+++ tools/BPobs.py
@@ -67,20 +67,21 @@ class Trainer:
         self.sess.close()
         return lossalls, y_preds
 ###########################################################################
-def figlossandpred(lossalls, y_preds, X, Y, Z, expdir, randseed, maxstep, outitvl, ylines, ylim):
+def figlossandpred(lossalls, y_preds, X, Y, Z, expdir, randseed, maxstep, outitvl, ylines, ylimmin, ylimmax):
     plt.figure(11)
     start = 0
     plt.plot(np.arange(start, len(lossalls), 1),lossalls[start:],color='r',alpha=0.5)
     for yline in ylines:
         plt.axhline(y=yline, linewidth=0.5, color='k')
-    plt.ylim([0.0,ylim])
+    plt.yscale('log')
+    plt.ylim([ylimmin,ylimmax])
     for i in range(outitvl):
         xline = int(maxstep/outitvl)*(i+1)
         plt.axvline(x=xline, linewidth=0.5, color='k')
     plt.savefig(expdir+'loss'+str(randseed)+".png", figsize=(16, 9), dpi=300, facecolor="azure", bbox_inches='tight', pad_inches=0)
     axp = Axes3D(plt.figure(12))
     Zp  = np.array(y_preds).reshape(Z.shape)
-    axp.plot_surface(X,Y,Zp, rstride=1, cstride=1, cmap='rainbow', alpha=0.5) 
+    axp.plot_surface(X,Y,Zp, rstride=1, cstride=1, cmap='rainbow', alpha=0.3) 
     plt.savefig(expdir+'pred'+str(randseed)+".png", figsize=(16, 9), dpi=300, facecolor="azure", bbox_inches='tight', pad_inches=0)
 def setsession(randseed):
     tfconfig = tf.ConfigProto(device_count={'GPU': 0})
@@ -106,18 +107,25 @@ def targetfunction(acc,lim,freq,randflag):
     return X,Y,Z,x_train,y_train
 ###########################################################################
 def main():
-    acc, lim, freq, randflag = 0.025, 2.0, 10.0, False
-    X,Y,Z,x_train,y_train = targetfunction(acc=acc,lim=lim,freq=freq,randflag=randflag)
-
     randseed = int(sys.argv[1])
     sess = setsession(randseed)
+    hyps = sys.argv[3].split('|')
+    modelname = hyps[0]
+    acc, lim, freq, randflag = float(hyps[1]), float(hyps[2]), 10.0, False
+    unitslist= [int(num) for num in hyps[3].split('-')]
+    dt, maxstep, outitvl = float(hyps[4]), int(hyps[5]), 3
+    limmin, limmax, nylines, nlogbins = 5e-5, 5e-2, 10, 50
+    print(sys.argv[1],':',sys.argv[2],':',sys.argv[3])
+    expdir = sys.argv[3]+'/'#str(acc).replace('.','-')+'+'+str(unitslist).replace('[','-').replace(']','-')+'+'+str(dt).replace('.','-')+'+'+str(maxstep)+'/'
+    if not os.path.exists(expdir): os.makedirs(expdir)
 
-    unitslist= [2,4,1]
+    #acc, lim, freq, randflag = 0.025, 2.0, 10.0, False
+    X,Y,Z,x_train,y_train = targetfunction(acc=acc,lim=lim,freq=freq,randflag=randflag)
+
+    #unitslist= [2,4,1]
     model = Model(x_train,y_train,unitslist)
 
-    dt, maxstep, outitvl = 0.1, 1000, 3
-    expdir = str(acc).replace('.','-')+'+'+str(unitslist).replace('[','-').replace(']','-')+'+'+str(dt).replace('.','-')+'+'+str(maxstep)+'/'
-    if not os.path.exists(expdir): os.makedirs(expdir)
+    #dt, maxstep, outitvl = 0.1, 1000, 3
     trainer = Trainer(x_train,y_train,sess,model,expdir)
     lossalls, y_preds = trainer.fit(dt,maxstep,outitvl)
 
@@ -129,17 +137,27 @@ def main():
     lossmins = [float(lossmin) for lossmin in lossmins]
     flossmins.close()
     if lossmins[-1]<=min(lossmins) or len(lossmins)<10:
-        ylines, ylim = [0.01,0.005,0.004,0.003,0.002,0.001,0.0005], 0.02
-        figlossandpred(lossalls, y_preds, X, Y, Z, expdir, randseed, maxstep, outitvl, ylines, ylim)
+        ylines, ylimmin, ylimmax = np.logspace(np.log10(limmin),np.log10(limmax),nylines), limmin, limmax #[1e-2,5e-3,4e-3,3e-3,2e-3,1e-3,5e-4,4e-4,3e-4,2e-4,1e-4]
+        figlossandpred(lossalls, y_preds, X, Y, Z, expdir, randseed, maxstep, outitvl, ylines, ylimmin, ylimmax)
 
-    print(sys.argv[1],':',sys.argv[2])
     if sys.argv[1]==sys.argv[2] or int(sys.argv[1])%100==0:
         plt.figure(111)
-        plt.hist(lossmins, bins=np.arange(0, max(lossmins)+0.0001, 0.0001), normed=False,facecolor='r',edgecolor='r',hold=0,alpha=0.2)
-        plt.savefig(expdir+'lossmins'+".png", figsize=(16, 9), dpi=300, facecolor="azure", bbox_inches='tight', pad_inches=0)
-        ax = Axes3D(plt.figure(112))
-        ax.plot_surface(X, Y, Z, rstride=1, cstride=1, cmap='rainbow', alpha=0.2)
-        plt.savefig(expdir+'gold'+".png", figsize=(16, 9), dpi=300, facecolor="azure", bbox_inches='tight', pad_inches=0)
+        xlimmin, xlimmax = min(lossmins+[limmin]) ,max(lossmins+[limmax])
+        logbins = np.logspace(np.log10(xlimmin),np.log10(xlimmax),nlogbins) #bins = np.arange(xlimmin, xlimmax, 1e-6) #linspace
+        plt.hist(lossmins, bins=logbins, normed=False,facecolor='r',edgecolor='r',hold=0,alpha=0.2)
+        plt.xscale('log')
+        numparas = np.sum([np.prod(v.get_shape().as_list()) for v in tf.trainable_variables()])
+        figname = sys.argv[3]+'|lossmins|'+str(x_train.shape[0])+'+'+str(numparas)+'|'+str(min(lossmins))+'+'+str(np.median(lossmins))+".png"
+        plt.savefig(figname, figsize=(16, 9), dpi=300, facecolor="azure", bbox_inches='tight', pad_inches=0)
+        fresults=open('dohypersresults','a')
+        print(sys.argv[3]+','+str(x_train.shape[0])+','+str(numparas)+','+str(min(lossmins))+','+str(np.median(lossmins)),file=fresults)
+        fresults.close()
+
+        goldname = str(acc)+'+'+str(lim)+'+'+str(freq)+'gold'+".png"
+        if not os.path.exists(goldname):
+            ax = Axes3D(plt.figure(112))
+            ax.plot_surface(X, Y, Z, rstride=1, cstride=1, cmap='rainbow', alpha=0.2)
+            plt.savefig(goldname, figsize=(16, 9), dpi=300, facecolor="azure", bbox_inches='tight', pad_inches=0)
 
 if __name__ == '__main__':
     main()
